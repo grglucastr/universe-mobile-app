962cb5f4138dc81a5bfe2ca4cf2657814d2aae85
= <george.bentes@gmail.com>
2023-05-25 21:55:35 -0300

    Finish list all in the screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:universe_mobile_app/components/not_found_item.dart';
 import 'package:universe_mobile_app/http/webclient.dart';
 import 'package:universe_mobile_app/models/planet.dart';
 
@@ -11,23 +12,6 @@ class UniverseMobileApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Planet> planets = List.empty(growable: true);
-
-    planets.add(Planet(0, "planet x", 2131.22));
-    planets.add(Planet(0, "planet x", 2131.22));
-    planets.add(Planet(0, "planet x", 2131.22));
-    planets.add(Planet(0, "planet x", 2131.22));
-    planets.add(Planet(0, "planet x", 2131.22));
-    planets.add(Planet(0, "planet x", 2131.22));
-    planets.add(Planet(0, "planet x", 2131.22));
-    planets.add(Planet(0, "planet x", 2131.22));
-    planets.add(Planet(0, "planet x", 2131.22));
-    planets.add(Planet(0, "planet x", 2131.22));
-    planets.add(Planet(0, "planet x", 2131.22));
-    planets.add(Planet(0, "planet x", 2131.22));
-    planets.add(Planet(0, "planet x", 2131.22));
-    planets.add(Planet(0, "planet x", 2131.22));
-
     return MaterialApp(
       theme: ThemeData(
           appBarTheme: const AppBarTheme(
@@ -40,39 +24,75 @@ class UniverseMobileApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Universe'),
         ),
-        body: FutureBuilder<List<Planet>>(
-          future: findAll(),
-          builder: (context, snapshot){
-            switch(snapshot.connectionState){
-              case ConnectionState.none:
-                break;
-              case ConnectionState.waiting:
-                break;
-              case ConnectionState.active:
-                break;
-              case ConnectionState.done:
-                if(snapshot.hasError){
-                  return const Text('Something wrong happened during request.');
-                }
-                final List<Planet> planets  = snapshot.data!;
-                return _renderWidgetOfPlanets(planets);
-            }
-            return const Text('Unknow error');
-          }
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const TextField(
+                decoration: InputDecoration(
+                  labelText:
+                      'Search for planets, constellations, stars, satellites, etc.',
+                  hintText: 'Kleper X983',
+                  hintStyle: TextStyle(
+                    fontSize: 13.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Column(
+                  children: [
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Universe items',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    FutureBuilder<List<Planet>>(
+                        future: findAll(),
+                        builder: (context, snapshot) {
+                          switch (snapshot.connectionState) {
+                            case ConnectionState.none:
+                              break;
+                            case ConnectionState.waiting:
+                              break;
+                            case ConnectionState.active:
+                              break;
+                            case ConnectionState.done:
+                              if (snapshot.hasError) {
+                                return const Text(
+                                    'Something wrong happened during request.');
+                              }
+                              final List<Planet> planets = snapshot.data!;
+                              return _renderWidgetOfPlanets(planets);
+                          }
+                          return const Text('Unknow error');
+                        }),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _renderWidgetOfPlanets(final List<Planet> planets){
-    if(planets.isEmpty){
-      return const Text('No planet found in your universe.');
+  Widget _renderWidgetOfPlanets(final List<Planet> planets) {
+    if (planets.isEmpty) {
+      return const NotFoundItem(
+          customText: 'No planets found in your universe.');
     }
     return _renderListOfPlanets(planets);
   }
 
   ListView _renderListOfPlanets(List<Planet> planets) {
     return ListView.separated(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
       itemCount: planets.length,
       separatorBuilder: (context, index) => const Divider(
         color: Colors.black,
